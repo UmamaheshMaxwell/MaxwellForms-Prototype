918604fddb27002b77232dfe342041e3bba8d4f4
UmamaheshMaxwell <maheshmeka16@gmail.com>
2016-04-02 15:10:56 +0530

    Resturctures the folder

---
+++ config/config.js
@@ -0,0 +1,8 @@
+
+module.exports = { 
+
+  HELLOSIGN_USERNAME : 'maheshmeka16@gmail.com',
+  HELLOSIGN_PASSWORD : 'JOHNGALT123$',
+  HELLOSIGN_CLIENTID : 'ad00a6dc8ff93743a45ad90c150d1453',
+  HELLOSIGN_APIKEY   : 'c98f09c4617ff75700ed68abe46ab5d4cd4fa7a1cc63feb33c1af25707a75fc5'
+};
